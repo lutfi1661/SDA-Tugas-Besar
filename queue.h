0d75fd14854b51ce19b9f7dddfcf4e137f627843
Kevandra12 <82016847+Kevandra12@users.noreply.github.com>
2021-07-25 21:20:24 +0700

    Update queue.h

--- queue.h
+++ queue.h
@@ -1,5 +1,5 @@
 /*
-	Modified		: Algi Fari Ramdhani & Mochammad Lutfi Faturachman
+	Modified		: Algi Fari Ramdhani, Mochammad Lutfi Faturachman & Kevandra Rivaldy Adrianto Putra
 	Date Modified 	: 19 Juli 2021
 	Version			: 1.0
 */
@@ -84,20 +84,45 @@ mengirimkan 0 jika Q kosong
 */
 int NBElmt(Queue Q);
 
+/*	
+  Menampilkan daftar penyakit yang diambil dari variabel global ArrDisease
+*/
 void PrintDisease();
 
+/*
+  Mengembalikan nilai string apakah Ringan, Sedang atau Berat berdasarkan penyakit yang diderita
+*/
 char *CategoryCheck(int Disease);
 
+/*
+  Mengembalikan nilai integer berupa waktu pemeriksaan berdasarkan kategori penyakit
+*/
 int InspectionTimeCheck(int Disease);
 
+/*
+  Mengembalikan nilai integer berupa perhitungan nilai prioritas berdasarkan jumlah penyakit dan kategori penyakit
+*/
 int PriorityCount(int Ringan, int Sedang, int Berat);
 
+/*
+  Mengembalikan nilai integer untuk waktu pemeriksaan berdasarkan setiap penyakit yang diderita
+*/
 int InspectionTimeCount(int Ringan, int Sedang, int Berat);
 
+/*	
+  Menentukan dan mengubah waktu mulai dan waktu selesai jika terjadi perubahan urutan antrian berdasarkan nilai prioritas
+*/
 void setTime(Queue *Q, infotype *X);
 
+/*	
+  Menampilkan menu registrasi dan menerima masukan pengguna yang akan dimasukkan ke dalam Queue
+*/
 void Registration(Queue *Q);
 
+/*	
+  Menampilkan daftar antrian yang memuat informasi total antrian, no urut antrian, nama, waktu kedatangan, 
+  daftar penyakit, prioritas, waktu pemeriksaan, waktu mulai dan waktu selesai
+*/
 void PrintQueue(Queue Q);
 
 #endif // QUEUE_H
