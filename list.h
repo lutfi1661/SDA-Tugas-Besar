bdd0b4bc4d02194fb298c7ee13e34d2891586595
lutfi1661 <lutfi16.mlf@gmail.com>
2021-08-05 00:02:07 +0700

    Update list.h

--- list.h
+++ list.h
@@ -3,12 +3,12 @@
 /* 	   fisik pointer; 				*/
 /* 	   Representasi address dgn pointer 		*/
 /* Oleh :  ANI RAHMANI / 23501007 	  		*/
-/* Tgl  :  25/10/01					*/	 
+/* Tgl  :  25/10/01					*/
 
 /* List :  */
 
 #ifndef list1_H
-#define list1_H 
+#define list1_H
 
 #include "boolean.h"
 #include <stdlib.h>
@@ -23,7 +23,7 @@ typedef struct{
 	char Category[10];
 	int InspectionTime;
 }InfoList;
-                                     
+
 typedef struct tElmtlist *address;
 typedef struct  tElmtlist   {
 	InfoList info;
@@ -41,7 +41,7 @@ typedef struct {
 
 /* ** Prototype   **  */
 /* ** Test List Kosong **  */
-boolean ListEmpty(List  L);                        
+boolean ListEmpty(List  L);
 /*  Mengirim true jika List kosong  */
 
 /* Pembuatan  List Kosong  */
@@ -67,16 +67,8 @@ void InsVLast(List *L, InfoList X);
 /* I.S  : L mungkin kosong          */
 /* F.S  : Melakukan alokasi sebuah elemen dan                */
 /*        menambahkan elemen list di akhir; elemen terakhir  */
-/*        yang baru bernilai X jika alokasi berhasil, 	     */	
+/*        yang baru bernilai X jika alokasi berhasil, 	     */
 /*	  Jika alokasi gagal: I.S = F.S			     */
-
-/* *** Penghapusan Elemen  ***  */
-	  
-
-void DelVFirst(List *L, InfoList *X);
-  /* I.S    : List tidak kosong  		                 */
-  /* F.S    : Elemen pertama List dihapus, nilai info disimpan   */
-  /*	      pada X dan alamat elemen pertama  di-dealokasi 	 */
 
 /* *******  PRIMITIF BERDASARKAN ALAMAT ********	*/
 /* Penambahan Elemen Berdasarkan Alamat 		*/
@@ -84,22 +76,5 @@ void DelVFirst(List *L, InfoList *X);
 void InsertLast(List  *L, address P);
 /* I.S   : Sembarang, P sudah dialokasi				*/
 /* F.S   : P ditambahkan  sebagai elemen terakhir yang baru	*/
-
-/* ****** PENGHAPUSAN SEBUAH ELEMEN	*********   */
-void DelFirst (List *L, address *P);
-/* I.S   : List tidak kosong 	*/
-/* F.S   : P adalah alamat elemen pertama list sebelum penghapusan */
-/*         Elemen List berkurang satu (mungkin menjadi kosong)     */
-/* First elemen yang baru adalah suksesor elemen pertama yang lama */
-
-/* *************PROSES SEMUA ELEMEN ****************   */
-void PrintInfo(List L, char **A);
-/* I.S   : List mungkin kosong 	*/
-/* F.S   : Jika list tidak kosong, semua info yang disimpan pada */
-/*         elemen list di-print					 */
-/*         Jika list kosong, hanya menuliskan "List Kosong"	 */
-
-int NbElmt(List L);
-/* Mengirimkan banyaknya elemen list, mengirimkan Nol jika kosong */
 
 #endif
